f36a6233eb47e5204efde1b9a7fc3ad80dbdfad3
GnuT 268 <dtroyal1102@gmail.com>
2022-01-05 10:39:21 +0700

    sd toan tu

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhap chieu rong");
+inputHeight = prompt("nhap chieu dai");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Dien tich la: " + area);
