ee73c137b3b5ec3d42e4d136a6a6bfee9842bd20
GnuT 268 <dtroyal1102@gmail.com>
2022-01-05 14:09:12 +0700

    bai tap 1

---
+++ BTbai1.js
@@ -0,0 +1,8 @@
+let diemVatly = 8
+let diemHoahoc = 5
+let diemSinhhoc = 6
+let diemTB = (diemVatly + diemHoahoc + diemSinhhoc)/3
+let tong = diemVatly + diemHoahoc + diemSinhhoc
+document.write("Diem trung binh bang " + diemTB)
+document.write('<br/>')
+document.write('Tong diem bang ' + tong)
